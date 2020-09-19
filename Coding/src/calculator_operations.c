499fd99be6df9f9c659c0ed7a901d795a5ea8a0c
99002578 <71481103+99002578@users.noreply.github.com>
2020-09-19 10:45:12 +0530

    Update calculator_operations.c

--- Coding/src/calculator_operations.c
+++ Coding/src/calculator_operations.c
@@ -32,17 +32,6 @@ for (c = 1; c <= operand1; c++)
     r = r * c;
 return r;
  }
-int hcf(int operand1, int operand2)
-{
-    
-    int max = (operand1 > operand2) ? operand1 : operand2;
 
-    while (1) {
-        if (max % operand1 == 0 && max % operand2 == 0) {
-            printf("The LCM of %d and %d is %d.", operand1, operand2, max);
-            break;
-        }
-        ++max;
-    }
-    return 0;
-}
+  
+
